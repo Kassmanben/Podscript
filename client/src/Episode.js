4f3eeb70fd3a0020cab7eee98c679fad97e83de6
KassmanBen <kassmanben@gmail.com>
2018-09-15 16:24:45 -0700

    fixed formatting for mobile

--- client/src/Episode.js
+++ client/src/Episode.js
@@ -37,29 +37,31 @@ class Episode extends Component {
         <div>
         {(!this.state.displayAll ? 
         
-        <div key={`${id}-episode-id`} className='row episode' onClick={this.showMoreorLess}>
-            <img className='img-thumb' src={require('./MBMBaM.jpg')} alt="My Brother, My Brother and Me Logo"></img>
-            <div className="row col-10">
+        <div key={`${id}-episode-id`} className='row episode'>
+            <div className="col-xs-2">
+            <img className='img-thumb img-fluid' src={require('./MBMBaM.jpg')} alt="My Brother, My Brother and Me Logo"></img>
+            </div>
+            <div className="col-7">
                 <div className="episode-info">
-                    <div className='row name'>{EpisodeTitle}</div>
+                    <div className='row name text-truncate'>{EpisodeTitle}</div>
                 </div>
-                <div id={`${id}-completed`} className="completed">
-                    <FontAwesomeIcon icon={this.state.completed === "0" ? "times-circle":"check-circle"} 
-                        color={this.state.completed === "0" ? "#a91212":"#2e9e2e"} size="2x" vertical-align="middle"/>
+                <div id="cool-hover" className="row col-12" onClick={this.showMoreorLess}>
+                    <FontAwesomeIcon className="hideorshow" icon={this.state.moreOrLess === "+" ? "chevron-down":"chevron-up"} vertical-align="middle"/>
                 </div>
             </div>
-                <div id="cool-hover" className="row hover col-12">
-                    <FontAwesomeIcon className="hideorshow" icon={this.state.moreOrLess === "+" ? "chevron-down":"chevron-up"} vertical-align="middle"/>
+                <div id={`${id}-completed`} className="completed col-1">
+                    <FontAwesomeIcon icon={this.state.completed === "0" ? "times-circle":"check-circle"} 
+                        color={this.state.completed === "0" ? "#a91212":"#2e9e2e"} size="2x" vertical-align="middle"/>
                 </div>
         </div>
         
         : 
 
-        <div key={`${id}-episode-id`} className='row episode-expanded' onClick={this.showMoreorLess}>
+        <div key={`${id}-episode-id`} className='row episode-expanded' >
         <div className="row">
 
-            <div className="col-4">
-                <img className='img-expanded row center-block' src={require('./MBMBaM.jpg')} alt="My Brother, My Brother and Me Logo"></img>
+            <div className="col-md-4">
+                <img className='img-expanded row center-block' src={require('./MBMBaM_lg.png')} alt="My Brother, My Brother and Me Logo"></img>
                 <div id={`${id}-completed`} className="completed-expanded row">
             <FontAwesomeIcon icon={this.state.completed === "0" ? "times-circle":"check-circle"} 
             color={this.state.completed === "0" ? "#a91212":"#2e9e2e"} size="2x" vertical-align="middle"/>
@@ -67,17 +69,16 @@ class Episode extends Component {
             </div>
             </div>
                 
-            <div className="episode-info col-7">
+            <div className="episode-info col-md-7">
                 <div className='row name-expanded'>{EpisodeTitle}</div>
-                <a className="more" onClick={this.showMoreorLess}>{this.state.moreOrLess}</a>
                 <div className="description row expanded">
                 {EpisodeDescription}
                 </div>
             </div>
         </div>
-        <div id="cool-hover" className="row hover col-12">
+        <div id="cool-hover" className="row col-md-12 padding-top" onClick={this.showMoreorLess}>
                     <FontAwesomeIcon className="hideorshow" icon={this.state.moreOrLess === "+" ? "chevron-down":"chevron-up"} vertical-align="middle"/>
-                </div>
+        </div>
           
         </div>
         )}</div>
